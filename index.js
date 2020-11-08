f4ffaf53db9db1d24ff7da641d881b39663dac16
lee-ma <leema9321@gmail.com>
2020-11-07 21:38:28 -0700

    express starter code

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+require('./routes/compileRoutes')(app);
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log("Running on port ", PORT)
+})
